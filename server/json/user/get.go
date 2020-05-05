13b447cdb2dbc57898c03edda60ebbb933d77b88
taichi0315 <k.taichi0315@gmail.com>
2020-05-05 19:21:02 +0900

    moved model to infrastructure

--- server/json/user/get.go
+++ server/json/user/get.go
@@ -1,11 +1,5 @@
 package user
 
-import "github.com/nepp-tumsat/documents-api/model"
-
 type UserGetResponse struct {
 	UserName string `json:"username"`
-}
-
-func ToUserGetResponse(user model.User) UserGetResponse {
-	return UserGetResponse{UserName: user.UserName}
 }
--- server/json/user/list.go
+++ server/json/user/list.go
@@ -1,6 +1,6 @@
 package user
 
-import "github.com/nepp-tumsat/documents-api/model"
+import "github.com/nepp-tumsat/documents-api/infrastructure/model"
 
 type UserListResponse struct {
 	Users []UserGetResponse `json:"users"`
@@ -10,7 +10,7 @@ func ToUserListResponse(users []model.User) UserListResponse {
 	var jsonUsers []UserGetResponse
 	var jsonUser UserGetResponse
 	for _, user := range users {
-		jsonUser = ToUserGetResponse(user)
+		jsonUser = UserGetResponse{UserName: user.UserName}
 		jsonUsers = append(jsonUsers, jsonUser)
 	}
 	return UserListResponse{Users: jsonUsers}
