e7bb5e7e41de8c68b9d48e38e0e28656bed4637a
taichi0315 <k.taichi0315@gmail.com>
2020-05-05 14:39:14 +0900

    success get request body

--- server/handler/auth/signup.go
+++ server/handler/auth/signup.go
@@ -1,15 +1,26 @@
 package auth
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/nepp-tumsat/documents-api/server/response"
+	"golang.org/x/xerrors"
 )
 
 func HandleAuthSignUp() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 
-		response.Success(writer, "success")
+		var requestBody authSignUpRequest
+		err := json.NewDecoder(request.Body).Decode(&requestBody)
+		if err != nil {
+			log.Printf("%+v\n", xerrors.Errorf("Error in json: %v", err))
+			response.BadRequest(writer, "Can't decode of json")
+			return
+		}
+
+		response.Success(writer, requestBody.Email)
 	}
 }
 
