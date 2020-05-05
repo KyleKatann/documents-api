2e20441cea581f77d18c4f490a2b62643a1aafaa
taichi0315 <k.taichi0315@gmail.com>
2020-05-05 17:15:10 +0900

    add documents data on dml

--- db/mysql/sql/dml.sql
+++ db/mysql/sql/dml.sql
@@ -1,5 +1,10 @@
 INSERT INTO users(id,username) VALUES ("user1","yaga");
 
-INSERT INTO user_auths(user_id,email,hash) VALUES ("user1","yaga@example.com","password");
+INSERT INTO user_auths(user_id,email,hash) VALUES ("user1","yaga@example.com","$2a$10$/z1SlkePlRKHfOOxb/w70.7B45svUsrqUq5kAFDUM/E4mjDMWvdwa");
 
 INSERT INTO auth_tokens(id,user_id,token,expiry) VALUES ("auth_token1","user1","token","2020-05-02 21:00:00");
+
+INSERT INTO documents(id,url,user_id) VALUES
+("document1","https://go-tour-jp.appspot.com/welcome/1","user1"),
+("document2","https://soudai.hatenablog.com/entry/2018/05/01/204442","user1"),
+("document3","https://angular.jp/tutorial","user1");
